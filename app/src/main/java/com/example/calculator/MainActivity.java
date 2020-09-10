be4461563fab0ce4b7cc9173628615175040ab64
Kyle Jordan <kjord021@fiu.edu>
2020-09-10 05:48:18 -0400

    Add Clear Code

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 if (specialty.equalsIgnoreCase("Clear")){
                     newNumber.setText("");
                     result.setText("");
-                    
+                    operand1 = null;
                 }
             }
         };
