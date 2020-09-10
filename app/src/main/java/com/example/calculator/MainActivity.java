b2ad951a5e3775b92e26fb5851983443d6454628
Kyle Jordan <kjord021@fiu.edu>
2020-09-10 06:31:29 -0400

    Refactor and Organization

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -18,15 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView displayOperation;
 
     //Variables to hold the operand and type of calculations
-    private Double operand1 = null;
+    private Double firstNumber = null;
     private String pendingOperation = "=";
 
     //array to hold numerical buttons
     ArrayList<Button> numericalButtons = new ArrayList<>();
-
     //array to hold operational buttons
     ArrayList<Button> operationalButtons = new ArrayList<>();
-
     //array to hold specialty buttons
     ArrayList<Button> specialtyButtons = new ArrayList<>();
 
@@ -40,47 +38,47 @@ public class MainActivity extends AppCompatActivity {
         displayOperation = (TextView) findViewById(R.id.operation);
 
         //get buttons that can be appended and add them to the array
-        Button button0 = (Button) findViewById(R.id.button0);
-        numericalButtons.add(button0);
-        Button button1 = (Button) findViewById(R.id.button1);
-        numericalButtons.add(button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        numericalButtons.add(button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        numericalButtons.add(button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        numericalButtons.add(button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        numericalButtons.add(button5);
-        Button button6 = (Button) findViewById(R.id.button6);
-        numericalButtons.add(button6);
-        Button button7 = (Button) findViewById(R.id.button7);
-        numericalButtons.add(button7);
-        Button button8 = (Button) findViewById(R.id.button8);
-        numericalButtons.add(button8);
-        Button button9 = (Button) findViewById(R.id.button9);
-        numericalButtons.add(button9);
-        Button buttonDot = (Button) findViewById(R.id.buttonDot);
-        numericalButtons.add(buttonDot);
+        Button btn0 = (Button) findViewById(R.id.btn0);
+        numericalButtons.add(btn0);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        numericalButtons.add(btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        numericalButtons.add(btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        numericalButtons.add(btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        numericalButtons.add(btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+        numericalButtons.add(btn5);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+        numericalButtons.add(btn6);
+        Button btn7 = (Button) findViewById(R.id.btn7);
+        numericalButtons.add(btn7);
+        Button btn8 = (Button) findViewById(R.id.btn8);
+        numericalButtons.add(btn8);
+        Button btn9 = (Button) findViewById(R.id.btn9);
+        numericalButtons.add(btn9);
+        Button btnDot = (Button) findViewById(R.id.btnDot);
+        numericalButtons.add(btnDot);
 
         //get buttons that perform operations and add to array
-        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
-        operationalButtons.add(buttonDivide);
-        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
-        operationalButtons.add(buttonMultiply);
-        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        operationalButtons.add(buttonMinus);
-        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        operationalButtons.add(buttonPlus);
-        Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
-        operationalButtons.add(buttonEquals);
-        final Button buttonNeg = (Button) findViewById(R.id.buttonNeg);
-        operationalButtons.add(buttonNeg);
-        buttonNeg.setEnabled(false);
+        Button btnDivide = (Button) findViewById(R.id.btnDivide);
+        operationalButtons.add(btnDivide);
+        Button btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        operationalButtons.add(btnMultiply);
+        Button btnSubtract = (Button) findViewById(R.id.buttonSubtract);
+        operationalButtons.add(btnSubtract);
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+        operationalButtons.add(btnAdd);
+        Button btnEquals = (Button) findViewById(R.id.btnEquals);
+        operationalButtons.add(btnEquals);
+        final Button btnNeg = (Button) findViewById(R.id.btnNeg);
+        operationalButtons.add(btnNeg);
+        btnNeg.setEnabled(false);
 
         //get specialty buttons
-        Button buttonClear = (Button) findViewById(R.id.buttonClear);
-        specialtyButtons.add(buttonClear);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
+        specialtyButtons.add(btnClear);
 
         //Define a listener for number button press
         View.OnClickListener numberListener = new View.OnClickListener() {
@@ -92,10 +90,10 @@ public class MainActivity extends AppCompatActivity {
                 newNumber.append(b.getText().toString());
 
                 if (newNumber.length() > 0){
-                    buttonNeg.setEnabled(true);
+                    btnNeg.setEnabled(true);
                 }
                 else{
-                    buttonNeg.setEnabled(false);
+                    btnNeg.setEnabled(false);
                 }
             }
         };
@@ -111,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 Button b = (Button) v;
                 String operation = b.getText().toString();
                 String value = newNumber.getText().toString();
-                buttonNeg.setEnabled(false);
+                btnNeg.setEnabled(false);
 
                 if (operation.equalsIgnoreCase("Neg")){
                     try {
@@ -153,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     newNumber.setText("");
                     result.setText("");
                     displayOperation.setText("");
-                    operand1 = null;
+                    firstNumber = null;
                 }
             }
         };
@@ -165,8 +163,8 @@ public class MainActivity extends AppCompatActivity {
 
         //retrieves the state on rotate
         if (savedInstanceState != null){
-            operand1 = savedInstanceState.getDouble("firstNumber");
-            result.setText(operand1.toString());
+            firstNumber = savedInstanceState.getDouble("firstNumber");
+            result.setText(firstNumber.toString());
         }
     }
 
@@ -178,8 +176,8 @@ public class MainActivity extends AppCompatActivity {
 
         displayOperation.setText(operation);
 
-        if (operand1 == null) {
-            operand1 = value;
+        if (firstNumber == null) {
+            firstNumber = value;
         }
         else {
 
@@ -188,28 +186,28 @@ public class MainActivity extends AppCompatActivity {
             }
             switch (pendingOperation) {
                 case "=":
-                    operand1 = value;
+                    firstNumber = value;
                     break;
                 case "/":
                     if (value == 0){
-                        operand1 = 0.0;
+                        firstNumber = 0.0;
                     }
                     else {
-                        operand1 /= value;
+                        firstNumber /= value;
                     }
                     break;
                 case "*":
-                    operand1 *= value;
+                    firstNumber *= value;
                     break;
                 case "-":
-                    operand1 -= value;
+                    firstNumber -= value;
                     break;
                 case "+":
-                    operand1 += value;
+                    firstNumber += value;
                     break;
             }
         }
-        result.setText(operand1.toString());
+        result.setText(firstNumber.toString());
         newNumber.setText("");
     }
 
@@ -219,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putDouble("firstNumber", operand1);
+        outState.putDouble("firstNumber", firstNumber);
     }
 
 }
